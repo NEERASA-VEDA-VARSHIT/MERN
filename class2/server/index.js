d4eda00e0f87bf420425acbf2b82fd02f7dfc504
nVedaVarshit <n.vedvarshit@gmail.com>
2025-08-29 07:05:07 +0530

    modified:   class2/server/index.js     route parameters

--- class2/server/index.js
+++ class2/server/index.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
+const courses = [
+    { course_id: 1, name: "Course 1", instructor: "Instructor 1" },
+    { course_id: 2, name: "Course 2", instructor: "Instructor 2" },
+    { course_id: 3, name: "Course 3", instructor: "Instructor 3" }
+]
+
 app.get('/', (req, res) =>{
     res.send("Home Page")
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
+})
+
+app.get('/courses/:course_id', (req, res) =>{
+    console.log(req.params)
+    let course = courses.find((course) => course.course_id === parseInt(req.params.course_id))
+    res.send(course)
 })
 
 // app.post()
